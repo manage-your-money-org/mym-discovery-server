6da3bf0c6cbe37483f415f4ba1837b511542071a
Rohit <rkumar8092378845@gmail.com>
2023-10-01 11:49:34 +0530

    added config client for getting the configuration from config server

--- src/test/java/com/rkumar0206/mymdiscoveryserver/MymDiscoveryServerApplicationTests.java
+++ src/test/java/com/rkumar0206/mymdiscoveryserver/MymDiscoveryServerApplicationTests.java
@@ -3,11 +3,11 @@ package com.rkumar0206.mymdiscoveryserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MymDiscoveryServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class MymDiscoveryServerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
